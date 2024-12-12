3d0d3689673a38f44ec6ce02c333e03e6d762284
lys <dldustj0112@gmail.com>
2024-12-12 10:45:33 +0900

    필요한것들 자동설치 쉘스크립트

---
+++ npm_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# npm 패키지들 설치치
+echo "필요 npm 패키지들 설치"
+for i in express mysql2 bcryptjs express-session body-parser mysql cors; do npm install $i; done
+
+# 설치 완료 메시지
+echo "npm 패키지 설치 완료"
